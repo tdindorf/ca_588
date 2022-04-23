64ec14d5720a21e0ac7539084267c2af784e5b31
tdindorf <t_dindorf@hotmail.com>
2022-04-23 11:16:53 +0200

    Update ca_588_all.10.js

--- ca_588_all.10.js
+++ ca_588_all.10.js
@@ -2,23 +2,23 @@ import {
   ca_588_2R_opts,
   ca_588_2R_parse,
   ca_588_2R_draw 
-} from  svg.ca_588_2R.10;
+} from  './modules/svg.ca_588_2R.10';
 
 import {
   ca_588_2R_arrays
-} from svg.ca_588_2R_arrays.10;
+} from './modules/svg.ca_588_2R_arrays.10';
 
 import {
   ca_588_TextBits,
   ca_588_Units,
   ca_588_Display_Data 
-} from  svg.ca_588_txt.10;
+} from  './modules/svg.ca_588_txt.10';
 
 import {
   mdl_svg_sizeMe,
   mdl_ca_divTxt,
   mdl_ca_arrayFltr
-} from mdl_ca_utils.31;
+} from './modules/mdl_ca_utils.31';
 
 // make main function public
 window.ca_588_2R_opts = ca_588_2R_opts
